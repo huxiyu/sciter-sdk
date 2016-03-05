93b293b552beffe759f80b41b49a08aafba9005d
huxiyu <huxiyu007@gmail.com>
2016-03-05 22:47:26 +0800

    完成OpenGL和Sciter的结合, 绘制三角形
    通过create child window , 路径: sciter-sdk\demos\sciter-opengl

--- include/behaviors/behavior_drawing-gdi.cpp
+++ include/behaviors/behavior_drawing-gdi.cpp
@@ -3,161 +3,253 @@
 #include "sciter-x-behavior.h"
 #include "sciter-x-graphics.hpp"
 
+#include <gl\gl.h>			// Header File For The OpenGL32 Library
+#include <gl\glu.h>			// Header File For The GLu32 Library
+
 namespace sciter
 {
-/*
-BEHAVIOR: gdi-drawing
-  - draws content layer using GDI primitives.
+	/*
+	BEHAVIOR: gdi-drawing
+	- draws content layer using GDI primitives.
 
-SAMPLE:
-   See: samples/behaviors/gdi-drawing.htm
-*/
+	SAMPLE:
+	See: samples/behaviors/gdi-drawing.htm
+	*/
 
-// dib (bitmap) combined with DC
+	// dib (bitmap) combined with DC
 
-class dib32
-  {
-    const unsigned _width;
-    const unsigned _height;
-    void*      _bits;
-    mutable HBITMAP    _old_bitmap;
-    mutable HDC        _dc;
-    HBITMAP    _bitmap;
+	class dib32
+	{
+		const unsigned _width;
+		const unsigned _height;
+		void*      _bits;
+		mutable HBITMAP    _old_bitmap;
+		mutable HDC        _dc;
+		HBITMAP    _bitmap;
 
-    BITMAPINFO _bitmap_info;
+		BITMAPINFO _bitmap_info;
 
-  public:
+	public:
 
-    dib32(unsigned w, unsigned h) :
-      _width(w),
-      _height(h),
-      _bits(0),
-      _old_bitmap(0),
-      _dc(0) {
+		dib32(unsigned w, unsigned h) :
+			_width(w),
+			_height(h),
+			_bits(0),
+			_old_bitmap(0),
+			_dc(0) {
 
-      memset(&_bitmap_info,0,sizeof(_bitmap_info));
-      _bitmap_info.bmiHeader.biSize = sizeof(_bitmap_info.bmiHeader);
-      _bitmap_info.bmiHeader.biWidth = _width;
-      _bitmap_info.bmiHeader.biHeight = 0 - int(_height);
-      _bitmap_info.bmiHeader.biPlanes = 1;
-      _bitmap_info.bmiHeader.biBitCount = 32;
-      _bitmap_info.bmiHeader.biCompression = BI_RGB;
+				memset(&_bitmap_info,0,sizeof(_bitmap_info));
+				_bitmap_info.bmiHeader.biSize = sizeof(_bitmap_info.bmiHeader);
+				_bitmap_info.bmiHeader.biWidth = _width;
+				_bitmap_info.bmiHeader.biHeight = 0 - int(_height);
+				_bitmap_info.bmiHeader.biPlanes = 1;
+				_bitmap_info.bmiHeader.biBitCount = 32;
+				_bitmap_info.bmiHeader.biCompression = BI_RGB;
 
-      _bitmap = ::CreateDIBSection(
-            NULL, // device context
-            &_bitmap_info,
-            DIB_RGB_COLORS,
-            &_bits,
-            0, // file mapping object
-            0); // file offset
+				_bitmap = ::CreateDIBSection(
+					NULL, // device context
+					&_bitmap_info,
+					DIB_RGB_COLORS,
+					&_bits,
+					0, // file mapping object
+					0); // file offset
 
-      if (0 == _bits) {
-        return;
-        //throw std::bad_alloc();
-      }
+				if (0 == _bits) {
+					return;
+					//throw std::bad_alloc();
+				}
 
-      memset(_bits,0, _width * _height * 4);
+				memset(_bits,0, _width * _height * 4);
 
-    }
+		}
 
-    ~dib32() {
-      if( _dc )
-      {
-        ::SelectObject(_dc,_old_bitmap);
-        ::DeleteDC(_dc);
-      }
-      if( _bitmap )
-        ::DeleteObject(_bitmap);
-    }
+		~dib32() {
+			if( _dc )
+			{
+				::SelectObject(_dc,_old_bitmap);
+				::DeleteDC(_dc);
+			}
+			if( _bitmap )
+				::DeleteObject(_bitmap);
+		}
 
-    void set_white()
-    {
-      memset(_bits,0xff, _width * _height * 4);
-    }
+		void set_white()
+		{
+			memset(_bits,0xff, _width * _height * 4);
+		}
 
-    unsigned  width() const { return _width; }
-    unsigned  height() const { return _height; }
-    void* bits() const { return _bits; }
-    BYTE* bytes() const { return (BYTE*)_bits; }
-    HDC   DC() const 
-    { 
-      if(!_dc) {
-        _dc = ::CreateCompatibleDC(NULL);
-        if (_dc)
-          _old_bitmap = (HBITMAP)::SelectObject(_dc,_bitmap);
-      }
-      return _dc; 
-    }
+		unsigned  width() const { return _width; }
+		unsigned  height() const { return _height; }
+		void* bits() const { return _bits; }
+		BYTE* bytes() const { return (BYTE*)_bits; }
+		HDC   DC() const 
+		{ 
+			if(!_dc) {
+				_dc = ::CreateCompatibleDC(NULL);
+				if (_dc)
+					_old_bitmap = (HBITMAP)::SelectObject(_dc,_bitmap);
+			}
+			return _dc; 
+		}
 
-    HBITMAP   detach() { auto r = _bitmap; _bitmap = 0; return r; }
+		HBITMAP   detach() { auto r = _bitmap; _bitmap = 0; return r; }
 
-    const BITMAPINFO& get_info() const { return _bitmap_info; }
-  };
+		const BITMAPINFO& get_info() const { return _bitmap_info; }
+	};
 
 
-struct gdi_drawing: public event_handler
-{
-    // ctor
-    gdi_drawing() {}
-    virtual ~gdi_drawing() {}
+	struct gdi_drawing: public event_handler
+	{
+		// ctor
+		gdi_drawing() {}
+		virtual ~gdi_drawing() {}
 
-    virtual bool subscription( HELEMENT he, UINT& event_groups )
-    {
-      event_groups = HANDLE_DRAW;  // it does drawing
-      return true;
-    }
+		virtual bool subscription( HELEMENT he, UINT& event_groups )
+		{
+			event_groups = HANDLE_DRAW;  // it does drawing
+			return true;
+		}
 
-    virtual void attached  (HELEMENT he ) { }
-    virtual void detached  (HELEMENT he ) { delete this; }
-    
-    virtual bool handle_draw   (HELEMENT he, DRAW_PARAMS& params ) 
-    {
-      if( params.cmd != DRAW_CONTENT ) return false; // drawing only content layer
+		virtual void attached  (HELEMENT he ) { 
+			printf("hello");
+		}
+		virtual void detached  (HELEMENT he ) { delete this; }
 
-      unsigned w = unsigned(params.area.right - params.area.left);
-      unsigned h = unsigned(params.area.bottom - params.area.top);
+		virtual bool handle_draw   (HELEMENT he, DRAW_PARAMS& params ) 
+		{
+			if( params.cmd != DRAW_CONTENT ) return false; // drawing only content layer
 
-      dib32 buffer(w,h);
+			unsigned w = unsigned(params.area.right - params.area.left);
+			unsigned h = unsigned(params.area.bottom - params.area.top);
 
-      do_draw(buffer.DC(), w,h);
+			dib32 buffer(w,h);
 
-      sciter::image img = sciter::image::create(w,h,false,buffer.bytes());
+			do_draw(buffer.DC(), w,h);
 
-      sciter::graphics gfx(params.gfx);
+			sciter::image img = sciter::image::create(w,h,false,buffer.bytes());
 
-      gfx.draw_image(&img, POS(params.area.left), POS(params.area.top));
+			sciter::graphics gfx(params.gfx);
 
-      return true;
-    
-    }
+			gfx.draw_image(&img, POS(params.area.left), POS(params.area.top));
 
-    void do_draw( HDC hdc, unsigned width, unsigned height ) {
-      RECT rc = { 0,0,width,height };
-      HBRUSH hbr1 = ::CreateSolidBrush(0x000000);
-      HBRUSH hbr2 = ::CreateSolidBrush(0x0000FF);
-      // fill it by black color:
-      ::FillRect(hdc,&rc,hbr1);
-      ::InflateRect(&rc,-40,-40);
-      ::FillRect(hdc,&rc,hbr2);
-      ::DeleteObject(hbr1);
-      ::DeleteObject(hbr2);
-    }
+			return true;
 
-};
+		}
 
-struct gdi_drawing_factory: public behavior_factory {
 
-  gdi_drawing_factory(): behavior_factory("gdi-drawing") { }
+		GLvoid ReSizeGLScene(GLsizei width, GLsizei height)		// Resize And Initialize The GL Window
+		{
+			if (height==0)										// Prevent A Divide By Zero By
+			{
+				height=1;										// Making Height Equal One
+			}
 
-  // the only behavior_factory method:
-  virtual event_handler* create(HELEMENT he) {
-    return new gdi_drawing();
-  }
+			glViewport(0,0,width,height);						// Reset The Current Viewport
 
-};
+			glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
+			glLoadIdentity();									// Reset The Projection Matrix
 
-// instantiating and attaching it to the global list
-gdi_drawing_factory gdi_drawing_factory_instance;
+			// Calculate The Aspect Ratio Of The Window
+			gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
+
+			glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
+			glLoadIdentity();									// Reset The Modelview Matrix
+		}
+
+		int InitGL(GLvoid)										// All Setup For OpenGL Goes Here
+		{
+			glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
+			glClearColor(1.0f, 0.0f, 0.0f, 0.5f);				// Black Background
+			glClearDepth(1.0f);									// Depth Buffer Setup
+			glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
+			glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
+			glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
+			return TRUE;										// Initialization Went OK
+		}
+
+		int DrawGLScene(GLvoid)									// Here's Where We Do All The Drawing
+		{
+			glClearColor(0.5,0.7,0.9,1.0); //指定背景颜色（依次为RGBA）
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
+			glLoadIdentity();									// Reset The Current Modelview Matrix
+			return TRUE;										// Everything Went OK
+		}
+
+		void do_draw( HDC hDC, unsigned w, unsigned h ) {
+
+			/*
+			RECT rc = { 0,0,w,h };
+			HBRUSH hbr1 = ::CreateSolidBrush(0x000000);
+			HBRUSH hbr2 = ::CreateSolidBrush(0x0000FF);
+			// fill it by black color:
+			::FillRect(hDC,&rc,hbr1);
+			::InflateRect(&rc,-40,-40);
+			::FillRect(hDC,&rc,hbr2);
+			::DeleteObject(hbr1);
+			::DeleteObject(hbr2);
+			*/
+
+			//HDC hDC=buffer.DC(); //获取一个DC，TForm1.Handle中保存有Form的窗口句柄
+			//调整该DC的象素格式
+
+			//本函数用于调整DC的象素格式，如缓冲区、颜色数等
+			//先不深究，只要知道它的作用就行
+			int nPixelFormat;
+			static	PIXELFORMATDESCRIPTOR pfd=				// pfd Tells Windows How We Want Things To Be
+			{
+				sizeof(PIXELFORMATDESCRIPTOR),				// Size Of This Pixel Format Descriptor
+				1,											// Version Number
+				PFD_DRAW_TO_WINDOW |						// Format Must Support Window
+				PFD_SUPPORT_OPENGL |						// Format Must Support OpenGL
+				PFD_DOUBLEBUFFER,							// Must Support Double Buffering
+				PFD_TYPE_RGBA,								// Request An RGBA Format
+				16,										// Select Our Color Depth
+				0, 0, 0, 0, 0, 0,							// Color Bits Ignored
+				0,											// No Alpha Buffer
+				0,											// Shift Bit Ignored
+				0,											// No Accumulation Buffer
+				0, 0, 0, 0,									// Accumulation Bits Ignored
+				16,											// 16Bit Z-Buffer (Depth Buffer)  
+				0,											// No Stencil Buffer
+				0,											// No Auxiliary Buffer
+				PFD_MAIN_PLANE,								// Main Drawing Layer
+				0,											// Reserved
+				0, 0, 0										// Layer Masks Ignored
+			};
+			// Choose a pixel format that best matches that described in pfd
+			nPixelFormat = ChoosePixelFormat(hDC, &pfd);
+			// Set the pixel format for the device context
+			SetPixelFormat(hDC, nPixelFormat, &pfd);
+
+			HGLRC hRC=wglCreateContext(hDC); //用这种DC去创建一个RC
+			wglMakeCurrent(hDC,hRC); //指定当前DC、当前RC为hDC、hRC
+
+			ReSizeGLScene(w, h);
+
+			if (!InitGL())									// Initialize Our Newly Created GL Window
+			{
+				MessageBox(NULL, L"Initialization Failed.", L"ERROR",MB_OK|MB_ICONEXCLAMATION);
+			}
+
+			//DrawGLScene();					// Draw The Scene
+			//SwapBuffers(hDC);				// Swap Buffers (Double Buffering)
+		}
+
+	};
+
+	struct gdi_drawing_factory: public behavior_factory {
+
+		gdi_drawing_factory(): behavior_factory("gdi-drawing") { }
+
+		// the only behavior_factory method:
+		virtual event_handler* create(HELEMENT he) {
+			return new gdi_drawing();
+		}
+
+	};
+
+	// instantiating and attaching it to the global list
+	gdi_drawing_factory gdi_drawing_factory_instance;
 
 
 }
--- include/behaviors/behavior_drawing.cpp
+++ include/behaviors/behavior_drawing.cpp
@@ -1,145 +1,410 @@
+
 #include "stdafx.h"
 #include "sciter-x.h"
 #include "sciter-x-behavior.h"
 #include "sciter-x-graphics.hpp"
 #include <time.h>   
 
+#include <gl\gl.h>			// Header File For The OpenGL32 Library
+#include <gl\glu.h>			// Header File For The GLu32 Library
+#include <time.h>
+
+// opengl func
+int InitGL(GLvoid);
+int DrawGLScene(GLvoid);
+GLvoid ReSizeGLScene(GLsizei width, GLsizei height)	;
+int fps;
+
+int InitGL(GLvoid)										// All Setup For OpenGL Goes Here
+{
+	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
+	glClearColor(1.0f, 0.0f, 0.0f, 0.5f);				// Black Background
+	glClearDepth(1.0f);									// Depth Buffer Setup
+	glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
+	glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
+	
+	return true;										// Initialization Went OK
+}
+
+int DrawGLScene(GLvoid)									// Here's Where We Do All The Drawing
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
+	glLoadIdentity();									// Reset The Current Modelview Matrix
+
+	glTranslatef(0.0f, 0.0f, -6.0f);      // 移入屏幕 6.0
+	glBegin(GL_TRIANGLES);       // 绘制三角形
+	glVertex3f( 0.0f, 1.0f, 0.0f);     // 上顶点
+	glVertex3f(-1.0f,-1.0f, 0.0f);     // 左下
+	glVertex3f( 1.0f,-1.0f, 0.0f);     // 右下
+	glEnd();        // 三角形绘制结束
+
+	return true;										// Everything Went OK
+}
+
+GLvoid ReSizeGLScene(GLsizei width, GLsizei height)		// Resize And Initialize The GL Window
+{
+	if (height==0)										// Prevent A Divide By Zero By
+	{
+		height=1;										// Making Height Equal One
+	}
+
+	glViewport(0,0,width,height);						// Reset The Current Viewport
+
+	glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
+	glLoadIdentity();									// Reset The Projection Matrix
+
+	// Calculate The Aspect Ratio Of The Window
+	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
+
+	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
+	glLoadIdentity();									// Reset The Modelview Matrix
+}
+
 namespace sciter
 {
-/*
-BEHAVIOR: native-clock
-  - draws content layer using sciter-x-graphics.hpp primitives.
+	/*
+	BEHAVIOR: native-clock
+	- draws content layer using sciter-x-graphics.hpp primitives.
+	o
+	SAMPLE:
+	See: samples/behaviors/native-drawing.htm
+	*/
 
-SAMPLE:
-   See: samples/behaviors/native-drawing.htm
-*/
+	HDC			hDC=NULL;		// Private GDI Device Context
+	HGLRC		hRC=NULL;		// Permanent Rendering Context
+	HINSTANCE hinst; 
+	HWND hwndMain=NULL;
+	HWND hwndChild=NULL;
+	BOOL	done=false;	
 
-struct native_clock: public event_handler
-{
-    // ctor
-    native_clock() {}
-    virtual ~native_clock() {}
-
-    virtual bool subscription( HELEMENT he, UINT& event_groups )
-    {
-      event_groups = HANDLE_DRAW   // it does drawing
-                   | HANDLE_TIMER; // and handles timer
-      return true;
-    }
-
-    virtual void attached  (HELEMENT he ) 
-    {
-      dom::element(he).start_timer(1000);
-    }
-    virtual void detached  (HELEMENT he ) { delete this; }
+	BOOL CreateGLWindow(HELEMENT he);
+	GLvoid KillGLWindow(GLvoid);
+	LRESULT CALLBACK ChildWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
+	DWORD SetWindows(LPRECT lpRect);
 
 
-    virtual bool handle_timer  (HELEMENT he,TIMER_PARAMS& params )
-    {
-      dom::element(he).refresh(); // refresh element's area
-      return true; // keep ticking
-    }
 
-    virtual bool handle_draw   (HELEMENT he, DRAW_PARAMS& params ) 
-    {
-      if( params.cmd != DRAW_CONTENT ) return false; // drawing only content layer
+	struct native_clock: public event_handler
+	{
+		// ctor
+		native_clock() {}
+		virtual ~native_clock() {}
 
-      const float PI = 3.141592653f;
+		virtual bool subscription( HELEMENT he, UINT& event_groups )
+		{
+			event_groups = HANDLE_DRAW   // it does drawing
+				| HANDLE_TIMER | HANDLE_KEY | HANDLE_SIZE; // and handles timer
+			return true;
+		}
 
-      float w = float(params.area.right - params.area.left);
-      float h = float(params.area.bottom - params.area.top);
+		virtual void attached  (HELEMENT he ) 
+		{
 
-      float scale = w < h? w / 300.0f: h / 300.0f;
+			CreateGLWindow(he);
+			dom::element(he).start_timer(1);
+		}
 
-      time_t rawtime;
-      time (&rawtime);
-      struct tm timeinfo = *localtime (&rawtime);
+		virtual void detached  (HELEMENT he ) {
 
-      sciter::graphics gfx(params.gfx);
-      gfx.state_save();
+			KillGLWindow();		
+			delete this; 
+		}
 
-      gfx.translate(params.area.left + w / 2.0f,params.area.top + h / 2.0f);
-      gfx.scale(scale,scale);    
-      gfx.rotate(-PI/2);
-      gfx.line_color(0);
-      gfx.line_width(8.f);
-      gfx.line_cap(SCITER_LINE_CAP_ROUND);
-       
-      // Hour marks
-      gfx.state_save();
-        gfx.line_color(gcolor(0x32,0x5F,0xA2));
-        for (int i = 0; i < 12; ++i) {
-          gfx.rotate(PI/6);
-          gfx.line(137.f,0,144.f,0);
-        }
-      gfx.state_restore();
 
-      // Minute marks
-      gfx.state_save();
-        gfx.line_width(3.f);
-        gfx.line_color(gcolor(0xA5,0x2A, 0x2A));
-        for (int i = 0; i < 60; ++i) {
-          if ( i % 5 != 0)
-            gfx.line(143,0,146,0);
-          gfx.rotate(PI/30.f);
-        }
-      gfx.state_restore();
+		virtual bool handle_timer  (HELEMENT he,TIMER_PARAMS& params )
+		{			
+			MSG		msg;	
 
-      int sec = timeinfo.tm_sec;
-      int min = timeinfo.tm_min;
-      int hr  = timeinfo.tm_hour;
-      hr = hr >= 12 ? hr - 12 : hr;
-  
-      // draw Hours
-      gfx.state_save();
-        gfx.rotate( hr*(PI/6) + (PI/360)*min + (PI/21600)*sec );
-        gfx.line_width(14);
-        gfx.line_color(gcolor(0x32,0x5F,0xA2));
-        gfx.line(-20,0,70,0);
-        gfx.state_restore();
+			// fps count
+			fps = 0;
+			int nbFrames = 0;
+			clock_t lastTime = clock();
 
-        // draw Minutes
-        gfx.state_save();
-        gfx.rotate( (PI/30)*min + (PI/1800)*sec );
-        gfx.line_width(10);
-        gfx.line_color(gcolor(0x32,0x5F,0xA2));
-        gfx.line(-28,0,100,0);
-      gfx.state_restore();
-      
-      // draw seconds
-      gfx.state_save();
-        gfx.rotate(sec * PI/30);
-        gfx.line_color(gcolor(0xD4,0,0));
-        gfx.fill_color(gcolor(0xD4,0,0));
-        gfx.line_width(6);
-        gfx.line(-30,0,83,0);
-        gfx.ellipse(0,0,10,10);
-    
-        gfx.fill_color(0);
-        gfx.ellipse(95,0,10,10);
-      gfx.state_restore();
-    
-      gfx.state_restore();
+			while(!done)									// Loop That Runs While done=FALSE
+			{
+				if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))	// Is There A Message Waiting?
+				{
+					if (msg.message==WM_QUIT)				// Have We Received A Quit Message?
+					{
+						done=TRUE;							// If So done=TRUE
+					}
+					else									// If Not, Deal With Window Messages
+					{
+						TranslateMessage(&msg);				// Translate The Message
+						DispatchMessage(&msg);				// Dispatch The Message
+					}
+				}
+				else										// If There Are No Messages
+				{
 
-      return false;
-    
-    }
+					clock_t currentTime  = clock();
+					nbFrames++;
+					//fps = float(n_frame)/(finish-start)*CLOCKS_PER_SEC;
+					long time = (currentTime - lastTime) / CLOCKS_PER_SEC;
+					if (  (currentTime - lastTime) / CLOCKS_PER_SEC >= 1.0) 
+					{
+						fps = nbFrames;
+						nbFrames = 0;
+						lastTime += CLOCKS_PER_SEC;
+					}
 
-};
+					DrawGLScene();					// Draw The Scene
+					SwapBuffers(hDC);				// Swap Buffers (Double Buffering)
+				}
+			}
 
-struct native_clock_factory: public behavior_factory {
+			dom::element(he).refresh(); // refresh element's area
+			return true; // keep ticking
+		}
 
-  native_clock_factory(): behavior_factory("native-clock") { }
+		virtual bool handle_draw   (HELEMENT he, DRAW_PARAMS& params ) 
+		{
+			//if( params.cmd != DRAW_CONTENT ) return false; // drawing only content layer
+			return true;
+		}
 
-  // the only behavior_factory method:
-  virtual event_handler* create(HELEMENT he) {
-    return new native_clock();
-  }
+		virtual bool handle_key    (HELEMENT he, KEY_PARAMS& params )
+		{
+			if ( VK_F1 == params.key_code )
+			{
+				printf("");
+			}
+			return true;
+		}
 
-};
+		virtual void handle_size  (HELEMENT he ) 
+		{
+			RECT rect;
+			// 获取本窗口客户区的RECT（矩形方框的四个边界点）
+			// 1064 585
+			GetClientRect(hwndChild,&rect);
+			ReSizeGLScene(rect.right - rect.left, rect.bottom - rect.top);	
+		}
 
-// instantiating and attaching it to the global list
-native_clock_factory native_clock_factory_instance;
+		//
+	};
 
+	struct native_clock_factory: public behavior_factory {
+
+		native_clock_factory(): behavior_factory("native-clock") { }
+
+		// the only behavior_factory method:
+		virtual event_handler* create(HELEMENT he) {
+			return new native_clock();
+		}
+
+	};
+
+	// instantiating and attaching it to the global list
+	native_clock_factory native_clock_factory_instance;
+
+
+
+	BOOL CreateGLWindow(HELEMENT he) 
+	{
+		hwndMain = dom::element(he).get_element_hwnd(false);
+
+		hinst	 = GetModuleHandle(NULL);				// Grab An Instance For Our Window
+
+		WNDCLASS   chd;//子窗口类   
+		chd.style         = CS_HREDRAW | CS_VREDRAW ; //创建窗体是加子(WS_CHILDWINDOW)窗体风格   
+		chd.lpfnWndProc   = ChildWndProc ; //修改项：换成指定的窗口过程   
+		chd.cbClsExtra    = 0 ;  
+		chd.cbWndExtra    = sizeof(long) ;  
+		chd.hInstance     = hinst ;  
+		chd.hIcon         = NULL;//修改项：不需要图标，设为NULL   
+		chd.hCursor       = LoadCursor (NULL, IDC_ARROW) ;  
+		chd.hbrBackground = (HBRUSH) GetStockObject (GRAY_BRUSH) ;//修改项：改为黑色   
+		chd.lpszMenuName  = NULL ;  
+		chd.lpszClassName = L"ChildClass"; // 窗口类名
+
+		// 注册窗口类
+		if(!RegisterClass(&chd))
+		{
+			printf("");
+		}
+
+		hwndChild = CreateWindow(
+			L"ChildClass",		//	窗口类
+			L"子窗口",	// 窗口的标题
+			// 窗口样式 ：可见，子窗口，可改变大小，具体窗口标题
+			WS_VISIBLE
+			| WS_CHILD 
+			// | WS_SIZEBOX 
+			// | WS_TILED 
+			,
+			// 默认大小和位置，后面使用SetWindows函数设置
+			CW_USEDEFAULT,	
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			hwndMain,		// 父窗口句柄
+			(HMENU)NULL,	// 没有菜单
+			hinst,		// 应用程序实例
+			NULL);		// 没有图标
+
+		RECT rect;
+		// 获取本窗口客户区的RECT（矩形方框的四个边界点）
+		GetClientRect(hwndMain,&rect);
+		// 设置子窗口的大小的位置
+		SetWindows(&rect);
+
+
+		// opengl 
+		GLuint		PixelFormat;			// Holds The Results After Searching For A Match
+		static	PIXELFORMATDESCRIPTOR pfd=				// pfd Tells Windows How We Want Things To Be
+		{
+			sizeof(PIXELFORMATDESCRIPTOR),				// Size Of This Pixel Format Descriptor
+			1,											// Version Number
+			PFD_DRAW_TO_WINDOW |						// Format Must Support Window
+			PFD_SUPPORT_OPENGL |						// Format Must Support OpenGL
+			PFD_DOUBLEBUFFER,							// Must Support Double Buffering
+			PFD_TYPE_RGBA,								// Request An RGBA Format
+			24,										// Select Our Color Depth
+			0, 0, 0, 0, 0, 0,							// Color Bits Ignored
+			0,											// No Alpha Buffer
+			0,											// Shift Bit Ignored
+			0,											// No Accumulation Buffer
+			0, 0, 0, 0,									// Accumulation Bits Ignored
+			16,											// 16Bit Z-Buffer (Depth Buffer)  
+			0,											// No Stencil Buffer
+			0,											// No Auxiliary Buffer
+			PFD_MAIN_PLANE,								// Main Drawing Layer
+			0,											// Reserved
+			0, 0, 0										// Layer Masks Ignored
+		};
+
+		if (!(hDC=GetDC(hwndChild)))							// Did We Get A Device Context?
+		{
+			KillGLWindow();								// Reset The Display
+			MessageBox(NULL, L"Can't Create A GL Device Context.", L"ERROR",MB_OK|MB_ICONEXCLAMATION);
+			return false;								// Return FALSE
+		}
+
+		if (!(PixelFormat=ChoosePixelFormat(hDC,&pfd)))	// Did Windows Find A Matching Pixel Format?
+		{
+			KillGLWindow();								// Reset The Display
+			MessageBox(NULL, L"Can't Find A Suitable PixelFormat.", L"ERROR",MB_OK|MB_ICONEXCLAMATION);
+			return false;								// Return FALSE
+		}
+
+		if(!SetPixelFormat(hDC,PixelFormat,&pfd))		// Are We Able To Set The Pixel Format?
+		{
+			KillGLWindow();								// Reset The Display
+			MessageBox(NULL, L"Can't Set The PixelFormat.", L"ERROR",MB_OK|MB_ICONEXCLAMATION);
+			return false;								// Return FALSE
+		}
+
+		if (!(hRC=wglCreateContext(hDC)))				// Are We Able To Get A Rendering Context?
+		{
+			KillGLWindow();								// Reset The Display
+			MessageBox(NULL, L"Can't Create A GL Rendering Context.", L"ERROR",MB_OK|MB_ICONEXCLAMATION);
+			return false;								// Return FALSE
+		}
+
+		if(!wglMakeCurrent(hDC,hRC))					// Try To Activate The Rendering Context
+		{
+			KillGLWindow();								// Reset The Display
+			MessageBox(NULL, L"Can't Activate The GL Rendering Context.", L"ERROR",MB_OK|MB_ICONEXCLAMATION);
+			return false;								// Return FALSE
+		}
+
+
+		// show window
+		// 显示窗口 
+		ShowWindow(hwndMain, SW_SHOWNORMAL); 
+		// ShowWindow(hWnd,SW_SHOW);						// Show The Window
+		UpdateWindow(hwndMain); 
+
+		SetForegroundWindow(hwndMain);						// Slightly Higher Priority
+		SetFocus(hwndMain);									// Sets Keyboard Focus To The Window
+
+		ReSizeGLScene(rect.right - rect.left, rect.bottom - rect.top);					// Set Up Our Perspective GL Screen
+
+		if (!InitGL())									// Initialize Our Newly Created GL Window
+		{
+			KillGLWindow();								// Reset The Display
+			MessageBox(NULL, L"Initialization Failed.", L"ERROR",MB_OK|MB_ICONEXCLAMATION);
+			return FALSE;								// Return FALSE
+		}
+
+		// attach
+		dom::element(he).attach_hwnd(hwndChild);
+		printf("");
+
+		return true;
+	}
+
+	GLvoid KillGLWindow(GLvoid)								// Properly Kill The Window
+	{
+		done = true;
+		/*
+		if (fullscreen)										// Are We In Fullscreen Mode?
+		{
+		ChangeDisplaySettings(NULL,0);					// If So Switch Back To The Desktop
+		ShowCursor(TRUE);								// Show Mouse Pointer
+		}
+		*/
+		if (hRC)											// Do We Have A Rendering Context?
+		{
+			if (!wglMakeCurrent(NULL,NULL))					// Are We Able To Release The DC And RC Contexts?
+			{
+				MessageBox(NULL, L"Release Of DC And RC Failed.", L"SHUTDOWN ERROR",MB_OK | MB_ICONINFORMATION);
+			}
+
+			if (!wglDeleteContext(hRC))						// Are We Able To Delete The RC?
+			{
+				MessageBox(NULL, L"Release Rendering Context Failed.", L"SHUTDOWN ERROR",MB_OK | MB_ICONINFORMATION);
+			}
+			hRC=NULL;										// Set RC To NULL
+		}
+
+		if (hDC && !ReleaseDC(hwndChild,hDC))					// Are We Able To Release The DC
+		{
+			MessageBox(NULL, L"Release Device Context Failed.", L"SHUTDOWN ERROR",MB_OK | MB_ICONINFORMATION);
+			hDC=NULL;										// Set DC To NULL
+		}
+
+		if (hwndChild && !DestroyWindow(hwndChild))					// Are We Able To Destroy The Window?
+		{
+			MessageBox(NULL, L"Could Not Release hWnd.", L"SHUTDOWN ERROR",MB_OK | MB_ICONINFORMATION);
+			hwndChild=NULL;										// Set hWnd To NULL
+		}
+
+		if (!UnregisterClass( L"ChildClass",hinst))			// Are We Able To Unregister Class
+		{
+			MessageBox(NULL, L"Could Not Unregister Class.", L"SHUTDOWN ERROR",MB_OK | MB_ICONINFORMATION);
+			hinst=NULL;									// Set hInstance To NULL
+		}
+	}
+
+	LRESULT CALLBACK ChildWndProc(HWND hwnd,
+		UINT uMsg,
+		WPARAM wParam,
+		LPARAM lParam
+		)
+	{
+		switch (uMsg) 
+		{ 
+		case WM_DESTROY: 
+			//KillGLWindow();		
+			//PostQuitMessage(0);
+			//    ExitThread(0);
+			return 0;
+		default: 
+			return DefWindowProc(hwnd, uMsg, wParam, lParam); 
+		} 
+	}
+
+	DWORD SetWindows(LPRECT lpRect)
+	{
+		// Child View
+		SetWindowPos(hwndChild, HWND_TOP,
+			lpRect->left, lpRect->top,
+			lpRect->right ,lpRect->bottom,
+			SWP_SHOWWINDOW);
+		return 0;
+	}
 
 }
